350d85b36d3c598c9330adb6963047c9ee3bd7cd
DaniilLysykh <lysykh_daniil@mail.ru>
2026-05-01 14:52:12 +0300

    устновка и регистрация axios

---
+++ frontend/shopClient/src/boot/axios.ts
@@ -0,0 +1,20 @@
+import { boot } from 'quasar/wrappers';
+import axios, { AxiosInstance } from 'axios';
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $axios: AxiosInstance;
+    $api: AxiosInstance;
+  }
+}
+
+// Указываем базовый URL нашего Java-сервера
+const api = axios.create({ baseURL: 'http://localhost:8080/api' });
+
+export default boot(({ app }) => {
+  // Позволяет использовать this.$axios и this.$api внутри Vue компонентов
+  app.config.globalProperties.$axios = axios;
+  app.config.globalProperties.$api = api;
+});
+
+export { api };
